be437ed15f645120ba2fcf53ab1f987e6bf53535
Chaitanya Anand <chaitanya.anand@atlan.com>
2025-05-31 23:52:15 +0530

    Update MobileAppDownload links to direct to Google Play Store; remove 'Coming Soon' text from Login and Signup components for a cleaner UI.

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -256,7 +256,7 @@ const Signup: React.FC = () => {
                       className="w-40"
                     />
                   </Link>
-                  <Link to="#" className="">
+                  <Link to="https://play.google.com/store/apps/details?id=com.bondbridge.bondbridgeonline" className="">
                     <img
                       src="/assets/stores/googleplay.svg"
                       alt="Get it on Google Play"
@@ -264,9 +264,6 @@ const Signup: React.FC = () => {
                     />
                   </Link>
                 </div>
-              </div>
-              <div className="text-4xl font-bold relative text-foreground -top-10 animate-pulse">
-                Coming Soon
               </div>
             </div>
           </form>
