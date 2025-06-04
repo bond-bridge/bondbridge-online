00cd627f26e8e0a5ed5167c9b6e5ad64e3dd0ca4
Suryansh Nagar <nagarsuryansh72@gmail.com>
2025-06-04 23:23:40 +0530

    playstore link added to login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -337,7 +337,7 @@ const Login: React.FC = () => {
                     className="w-40"
                   />
                 </Link>
-                <Link to="#" className="">
+                <Link to="https://play.google.com/store/apps/details?id=com.bondbridge.bondbridgeonline" className="">
                   <img
                     src="/assets/stores/googleplay.svg"
                     alt="Get it on Google Play"
