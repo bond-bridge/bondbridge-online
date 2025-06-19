4cc5dfd991a2ad4670032549fb03cc3f8b5e4ecc
Chaitanya Anand <chaitanya.anand@gmail.com>
2025-06-20 01:10:21 +0530

    app store

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -249,7 +249,7 @@ const Signup: React.FC = () => {
               <div className="flex justify-center pt-4">
                 {/* <p className="text-md text-center text-muted-foreground">Get the app:</p> */}
                 <div className="flex justify-center gap-4">
-                  <Link to="#" className="">
+                  <Link to="https://apps.apple.com/in/app/bondbridge-ai/id6745119162" className="">
                     <img
                       src="/assets/stores/appstore.svg"
                       alt="Download on App Store"
